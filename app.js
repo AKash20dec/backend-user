a624fbd3df70a365180634944b775e4fdc6b982d
AKash20dec <yakash20dec@gmail.com>
2024-06-03 21:47:33 +0530

    deploy

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: 'https://665de3366c2fed0a14c6aad0--userdashboard20.netlify.app'
 }));
 app.use(json());
 app.use(urlencoded({ extended: true }));
